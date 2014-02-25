7ecc46bd1c7065cefacfaf7c61ff4bb09595bb0c
toran billups <toranb@gmail.com>
2014-02-24 20:18:13 -0600

    registered the simple session model with breeze

--- website/static/website/js/app.js
+++ website/static/website/js/app.js
@@ -6,20 +6,32 @@ App.Router.map(function() {
 
 App.SessionsRoute = Ember.Route.extend({
     model: function() {
-        var sessions = [];
+        //move to an initializer of some kind (start)
         var ds = new breeze.DataService({
             serviceName: 'api',
             hasServerMetadata: false,
             useJsonp: false
         });
         var manager = new breeze.EntityManager({dataService: ds});
+        App.BreezeFactory(manager.metadataStore);
+        //move to an initializer (end)
+
         var query = new breeze.EntityQuery().from("sessions");
-        return manager.executeQuery(query).then(function(response){
-            response.results.forEach(function(pojo){
-                var session = Ember.Object.create(pojo);
-                sessions.pushObject(session);
-            });
-            return sessions;
+        return manager.executeQuery(query).then(function(response) {
+            return response.results;
+        });
+    }
+});
+
+App.BreezeFactory = Ember.Object.extend({
+    init: function(metadataStore) {
+        metadataStore.addEntityType({
+            shortName: "Session",
+            namespace: "App",
+            dataProperties: {
+                id:         { dataType: breeze.DataType.Int64, isPartOfKey: true },
+                name:       { dataType: breeze.DataType.String }
+            }
         });
     }
 });
