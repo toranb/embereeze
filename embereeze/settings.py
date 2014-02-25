2636fd5caae6c7eb0ff9a7a432f1e3c13484eacb
toran billups <toranb@gmail.com>
2014-02-24 19:40:33 -0600

    updated the django settings file

--- embereeze/settings.py
+++ embereeze/settings.py
@@ -1,33 +1,10 @@
-"""
-Django settings for embereeze project.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.6/topics/settings/
-
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.6/ref/settings/
-"""
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(s3vxmkef*@r2cwo^i*i!l!!#_ig*0rr##4h7r9t$y7ko9s39y'
-
-# SECURITY WARNING: don't run with debug turned on in production!
+SECRET_KEY = 'h2m=8ai(c1be5sfkjk=7%dh9p!!_$5fta!bk36vqh^^4+t%w=d'
 DEBUG = True
-
 TEMPLATE_DEBUG = True
-
 ALLOWED_HOSTS = []
-
-
-# Application definition
 
 INSTALLED_APPS = (
     'django.contrib.admin',
@@ -36,6 +13,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'api',
+    'website',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -48,35 +28,19 @@ MIDDLEWARE_CLASSES = (
 )
 
 ROOT_URLCONF = 'embereeze.urls'
-
 WSGI_APPLICATION = 'embereeze.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'data.db'),
     }
 }
 
-# Internationalization
-# https://docs.djangoproject.com/en/1.6/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
